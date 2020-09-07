ccf65e4a0d73a9d31ec7ece76cdf3a1f58cbbdc9
Antonio Paolillo <antonio.paolillo85@gmail.com>
2020-09-07 16:10:37 +0200

    Cleaning and fixing few details. Adding target features visualization.

--- python/features_extraction.py
+++ python/features_extraction.py
@@ -18,13 +18,11 @@ class features_extraction:
 
 class points_extraction(features_extraction):
 
-    def __init__(self):
+    def __init__(self, intrinsic):
         
         features_extraction.__init__(self)
 
-        self.blobs = np.array([])
-
-        # Vector of the visual features
+        # Vector of the visual features # OBSOLETE?!
         self.s = np.array([])
         
         # Setup of the opencv's SimpleBlobDetector parameters. 
@@ -65,7 +63,32 @@ class points_extraction(features_extraction):
         self.track = False
         self.counter_not_tracking = 10
 
-    def run(self,img):
+        # Initialize the center of the visual pattern with the center of the image
+        self.xm_old = intrinsic['cx']
+        self.ym_old = intrinsic['cy']  
+
+        self.blobs = np.array([])
+        
+        self.target = np.array([])
+        self.set_dummy_target(intrinsic)
+
+    def set_dummy_target(self,intrinsic):
+
+        offset = 50
+        dummy_array = np.array([
+                    intrinsic['cx']-offset, intrinsic['cy']-offset, intrinsic['cx']+offset, intrinsic['cy']-offset,
+                    intrinsic['cx']+offset, intrinsic['cy']+offset, intrinsic['cx']-offset, intrinsic['cy']+offset])
+        self.set_target(dummy_array)
+
+    def set_target(self,arr_in):
+
+        n_features = int(0.5*len(arr_in))
+
+        self.target = np.array([])
+        for k in range(0,n_features):
+            self.target = np.append(self.target,np.array([arr_in[2*k],arr_in[2*k+1]]))
+
+    def run(self, img):
 
         # Convert in gray scale image
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -75,7 +98,6 @@ class points_extraction(features_extraction):
 
         # Set-up the detector with default parameters.
         keypoints = self.detector.detect(gray_c)
-
         
         if len(keypoints)==4:
 
@@ -90,22 +112,29 @@ class points_extraction(features_extraction):
             yy4angles = [y - ym for y in yy] #0.5*height_frame 
             angles = np.arctan2(yy4angles,xx4angles)
             
-            #TODO: Fix this with arbitray number of features (not necessarily 4)
+            # Compute the distance travelled by the center of the visual pattern
+            distance = np.linalg.norm(np.array([xm,ym])-np.array([self.xm_old,self.ym_old]),2)
+            
+            # Keep memory of the previous visual pattern's center
+            self.xm_old = xm
+            self.ym_old = ym
+
             self.blobs = np.array([(angles[0], (xx[0],yy[0]), rr[0]),
                     (angles[1], (xx[1],yy[1]), rr[1]),
                     (angles[2], (xx[2],yy[2]), rr[2]),
                     (angles[3], (xx[3],yy[3]), rr[3])],
                     dtype=[('angle','f4'), ('center',np.float64, (2,)), ('ray','f4')])
             
-            # If you lost the detection at the previous iteration, sort the blob by the angle
-            # otherwise track the blobs with the same id than before, decided on the base of the euclidean distance
-            if self.track == True:
+            # If you did not loose the detection at the previous iteration and the pattern did not move much,
+            # track the blobs with the same id than before, decided on the base of the euclidean distance
+            if self.track == True and distance < 50:
                 blobs_aux = np.copy(self.blobs)
                 for (i,f) in enumerate(self.blobs):
                     dist = np.linalg.norm(f['center']-self.blobs_old['center'],2,1)
                     idx = np.argmin(dist)
                     blobs_aux[idx] = f
                 self.blobs = blobs_aux
+            # Otherwise, sort the blobs by their angle
             else:
                 self.blobs = np.sort(self.blobs,None,'quicksort','angle') 
 
@@ -137,11 +166,19 @@ class points_extraction(features_extraction):
             self.s = np.append(self.s, y)
             
             pt = (x,y)
-            cv2.circle(gray_c, pt, ray, color_track,  1, cv2.LINE_AA)
-            cv2.circle(gray_c, pt,   2, color_track, 10, cv2.LINE_AA)
+            #cv2.circle(gray_c, pt, ray, color_track,  1, cv2.LINE_AA)
+            cv2.circle(gray_c, pt, 8, color_track, -1, cv2.LINE_AA)
             
-            pt2 = (x+15,y+15)
-            cv2.putText(gray_c, str(i), pt2, cv2.FONT_HERSHEY_PLAIN, 0.8, color_track, 1, cv2.LINE_AA)        
+            pt2 = (x-4,y+4)#(x+15,y+15)
+            cv2.putText(gray_c, str(i), pt2, cv2.FONT_HERSHEY_PLAIN, 0.8, (255,190,120), 1, cv2.LINE_AA)        
+
+            if self.target.size>0:
+                x_red = int(self.target[2*i])
+                y_red = int(self.target[2*i+1])
+                red_color = (0,10,255)
+                cv2.circle(gray_c, (x_red,y_red), 8, red_color, 1, cv2.LINE_AA)
+                cv2.putText(gray_c, str(i), (x_red-4,y_red+4), cv2.FONT_HERSHEY_PLAIN, 0.8, red_color, 1, cv2.LINE_AA)   
+                
 
         self.image = gray_c
 
